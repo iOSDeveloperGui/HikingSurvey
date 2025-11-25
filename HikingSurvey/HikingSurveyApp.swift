76259c54dea53c734f82efaf647da7cfb9fd1f8d
iOSDeveloperGui <guilhermesilvadias535@gmail.com>
2025-11-25 15:36:38 -0300

    Initial Commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by iOS Developer on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
